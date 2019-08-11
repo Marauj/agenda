5ccd1c4db33f36508970699261b4862f8fe89931
jose neto <maraujneto@gmail.com>
2019-08-11 10:44:53 -0400

    Incluido função e tela de autenticação

--- agenda/urls.py
+++ agenda/urls.py
@@ -21,4 +21,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('agenda/', views.lista_eventos),
     path('', RedirectView.as_view(url='/agenda/')),
+    path('login/', views.login_user),
+    path('login/submit', views.submit_sogin),
+    path('logout/', views.logout_user)
 ]
