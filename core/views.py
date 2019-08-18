d3ec1e68dae7844f99c0e8d220be46d6db250881
jose neto <maraujneto@gmail.com>
2019-08-18 09:41:30 -0400

    Incluido template para inserção de eventos da agenda. Incluido finalmente o arquivo ignore para ignorar os arquivos compilados

--- core/views.py
+++ core/views.py
@@ -10,6 +10,22 @@ from django.contrib import messages
 def login_user(request):
     return render(request, 'login.html')
 
+@login_required(login_url='/login/')
+def evento(request):
+    return render(request, 'evento.html')
+
+@login_required(login_url='/login/')
+def submit_evento(request):
+    if request.POST:
+        titulo = request.POST.get('titulo')
+        data_evento = request.POST.get('data_evento')
+        descricao = request.POST.get('descricao')
+        usuario = request.user
+        Evento.objects.create(titulo=titulo,
+                              decricao=descricao,
+                              data_evento=data_evento,
+                              usuario=usuario)
+    return redirect('/')
 
 def submit_login(request):
     if request.POST:
